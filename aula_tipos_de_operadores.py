d2c6bc9cfc9ccece9153e7af04b8c31e60f2277c
Cristiano Santos <cjscristiano@gmail.com>
2024-04-30 11:44:56 -0300

    fim aula tipos de operadores

---
+++ aula_tipos_de_operadores.py
@@ -0,0 +1,28 @@
+##aula_tipos_de_operadores.py
+
+##principais operadores
+soma = 1 + 1
+print(soma)
+
+subtracao = 5 - 1
+print(subtracao)
+
+multiplicacao = 3 * 3
+print(multiplicacao)
+
+divisao = 5 / 2
+print(divisao)
+
+potencia = 2 ** 3
+print(potencia)
+
+## resto da divisao
+modulo = 10 % 3
+print(modulo)
+
+##expressoes e prioridades de execucao
+x = 10 - 5 * 2
+print(x)
+
+y = (10 - 5) * 2
+print(y)
